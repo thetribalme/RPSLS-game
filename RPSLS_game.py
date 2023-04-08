0d02f6830e0c4e8edb61e13ffeeb5a6fb7111973
thetribalme <thetribaleng@gmail.com>
2023-04-08 21:44:44 +0300

    added examination on an integer input for rounds to be played and for player move

--- RPSLS_game.py
+++ RPSLS_game.py
@@ -15,6 +15,7 @@ computer = random.choice(computer_list)
 count = 0
 PlayerWin = 0
 SheldonWin = 0
+fool = "Not the sharpest tool in the shed I see. You needed to type an integer."
 
 # game body
 print("What is your name? ")
@@ -31,28 +32,45 @@ if game == " ":
     exit()
 else:
     print("How many rounds wold you like to take?")
-rounds = int(input(" "))
-print("So let's begin then")
+rounds = input(" ")
+try:
+    tmp = int(rounds)
 
-while count < rounds:
-    index = int(input("Make your choice: 0-rock, 1-paper, 2-scissors, 3-lizard, 4-spock "))
-    player = player_list[index]
-    print(name + "'s move: " + player)
-    print("Sheldon's move: " + computer)
-    if win_move[player] == computer:
-        print("Sheldon: Oh, you won. According to the string theory that was just a coincidence")
-        PlayerWin = PlayerWin + 1
-    elif player == computer:
-        print("Sheldon: It's a tie. I don't acknowledge ties")
+    if int(rounds) > 20:
+        print("Ambitious, I like your scale. Hope you understand that you can not quit midway...")
+
+    print("So let's begin then")
+    while count < int(rounds):
+        index = int(input("Make your choice: 0-rock, 1-paper, 2-scissors, 3-lizard, 4-spock "))
+        try:
+            temp = index in range(5)
+            player = player_list[index]
+            print(name + "'s move: " + player)
+            print("Sheldon's move: " + computer)
+            if win_move[player] == computer:
+                print("Sheldon: Oh, you won. According to the string theory that was just a coincidence")
+                PlayerWin = PlayerWin + 1
+            elif player == computer:
+                print("Sheldon: It's a tie. I don't acknowledge ties")
+            else:
+                print("Sheldon: I won. Expected as much")
+                SheldonWin = SheldonWin + 1
+            count = count + 1
+        except:
+            print(fool + " Ant in range of listed above. Try again")
+
+    print("Oh well, that's unfortunate. We played only " + str(count) + " rounds")
+    if PlayerWin > SheldonWin:
+        print("Yours " + str(PlayerWin) + " out of " + str(rounds) + ". OK you won. It was nearly impossible but fine")
+    elif PlayerWin == SheldonWin:
+        print("Yours " + str(PlayerWin) + " out of " + str(rounds) + ". We're even. It'll be wise to play again")
     else:
-        print("Sheldon: I won. Expected as much")
-        SheldonWin = SheldonWin + 1
-    count = count + 1
-
-print("Oh well, that's unfortunate. We played only " + str(count) + " rounds")
-if PlayerWin > SheldonWin:
-    print(str(PlayerWin) + " out of " + str(rounds) + ". OK you won. It was nearly impossible but fine")
-elif PlayerWin == SheldonWin:
-    print(str(PlayerWin) + " out of " + str(rounds) + ". We're even. It'll be wise to play again")
-else:
-    print(str(PlayerWin) + " out of " + str(rounds) + ". Vini, vidi, vici. I won")
+        print("Yours " + str(PlayerWin) + " out of " + str(rounds) + ". Vini, vidi, vici. I won")
+    exit()
+except:
+    if count == 0:
+        print(
+            fool + "I don't think we can play when you make mistakes so early in the game, so... better luck nex time."
+                   "And a piece of advice, from me to you, grow some gray matter, will you?")
+    else:
+        exit()
